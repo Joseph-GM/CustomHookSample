2b64a3e90f91074c0531de230d2e05216ef671bb
Joseph-GM <jkim.english75@gmail.com>
2021-12-24 19:45:28 +0900

    making custom hook using Reducer

--- hooks/useInputs.js
+++ hooks/useInputs.js
@@ -1,17 +1,46 @@
 //import liraries
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useReducer } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-// create a component
+function reducer(form, action) {
+    switch(action.type) {
+        case 'CHANGE_INPUT':
+            return {
+                ...form,
+                [action.name]: action.value
+            }
+        case 'RESET_INPUT':
+            return action.initialForm
+        default:
+            return form;
+    }
+}
+
 function useInputs(initialForm) {
-    console.log("initialForm : ",initialForm);
-    const [form, setForm] = useState(initialForm);
-    console.log("form : ", form);
-    const onChnage = useCallback((keyvalue, e) => {
-        setForm (form => ({...form, [keyvalue]: e}));
-    }, [])
-    const reset = useCallback(() => setForm(initialForm), [initialForm])
-    return [form, onChnage, reset];
-};
+    const [form, dispatch] = useReducer(reducer, initialForm);
+    const onChange = useCallback((keyvalue, e) => {
+        dispatch({
+            type: 'CHANGE_INPUT',
+            name: keyvalue,
+            value: e,
+        })
+    })
+    const reset = useCallback(()=> dispatch({
+        type: 'RESET_INPUT',
+        initialForm
+    }), [initialForm])
+    return [form,onChange,reset]
+}
+// create a component
+// function useInputs(initialForm) {
+//     console.log("initialForm : ",initialForm);
+//     const [form, setForm] = useState(initialForm);
+//     console.log("form : ", form);
+//     const onChnage = useCallback((keyvalue, e) => {
+//         setForm (form => ({...form, [keyvalue]: e}));
+//     }, [])
+//     const reset = useCallback(() => setForm(initialForm), [initialForm])
+//     return [form, onChnage, reset];
+// };
 
 export default useInputs;
